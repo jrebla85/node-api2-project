8948f18fc4a4529e118bcf536ca73e1bd5d6aefa
jeremy black <jrebla85@yahoo.com>
2021-08-05 23:30:36 -0500

    index, server and route communicating, first GET endpoint working.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const Posts = require("./posts-model");
+const express = require("express");
+
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    Posts.find()
+    .then(posts => {res.status(200).json(posts)})
+    .catch(err => {
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    })
+});
+
+module.exports = router;
