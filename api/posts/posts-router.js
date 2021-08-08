7a1446f2df206e960febbed9ef297a37cf1444f6
jeremy black <jrebla85@yahoo.com>
2021-08-07 21:27:54 -0500

    GET post by ID endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -9,8 +9,23 @@ router.get("/", (req, res) => {
     Posts.find()
     .then(posts => {res.status(200).json(posts)})
     .catch(err => {
-        res.status(500).json({ message: "The posts information could not be retrieved" })
+        res.status(500).json({ message: "The posts information could not be retrieved", err })
     })
 });
+
+router.get("/:id", (req, res) => {
+    const {id} = req.params;
+    Posts.findById(id)
+    .then(post => {
+        if(!post){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }else{
+            res.status(200).json(post)
+        }
+    })
+    .catch(err=> {
+        res.status(500).json({ message: "The post information could not be retrieved", err })
+    })
+})
 
 module.exports = router;
