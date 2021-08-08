6d7de47332ebc0a4d8a707d2decc7db94b9f6a4c
jeremy black <jrebla85@yahoo.com>
2021-08-07 22:13:56 -0500

    Finished GET endpoint for comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -28,6 +28,22 @@ router.get("/:id", (req, res) => {
     })
 });
 
+router.get("/:id/comments", (req, res) => {
+    const {id} = req.params
+    const existingPost = Posts.findById(id)
+    .then(comment => {
+        if(!existingPost){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }else{
+            Posts.findPostComments(id);
+            res.status(200).json(comment)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The comments information could not be retrieved" })
+    })
+})
+
 router.post("/", (req, res) =>{
     const newPost = req.body;
     if(!newPost.title || !newPost.contents){
