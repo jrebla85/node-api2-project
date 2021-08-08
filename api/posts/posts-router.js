35f58fe44397567fbc9b0c63be57d82e876109a6
jeremy black <jrebla85@yahoo.com>
2021-08-07 21:54:50 -0500

    PUT endpoint completed. Error returned during testing saying that the updated post wasnt returned. Will Revisit. Moving on.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -35,12 +35,35 @@ router.post("/", (req, res) =>{
     }else{
         Posts.insert(newPost)
         .then(post => {
-            res.status(201).json({ message: post })
+            res.status(201).json(post)
         })
         .catch(err => {
             res.status(500).json({ message: "There was an error while saving the post to the database" })
         })
     }
 });
+
+router.put("/:id", async (req, res) =>{
+    const {id} = req.params
+    const changes = req.body
+
+    const existingPost = await Posts.findById(id)
+    try {
+        if(!existingPost){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }else{
+            if(!changes.title || !changes.contents){
+                res.status(400).json({ message: "Please provide title and contents for the post" })
+            }else{
+                const updatedPost = await Posts.update(id, changes) 
+                res.status(200).json(updatedPost)
+            }
+        }
+    }catch{
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+})
+
+//
 
 module.exports = router;
